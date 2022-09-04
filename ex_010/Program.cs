bd1bb84d04937e093a8238954dc22ea7d45c7162
Tatiana Kalinina <tankakalinka@gmail.com>
2022-09-04 20:13:13 +0300

    add hometask 10 SecondDigitFromThree

---
+++ ex_010/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Input any integer three-digit number: ");
+int number = int.Parse(Console.ReadLine());
+int digit2 = (number / 10) % 10;
+Console.WriteLine($"{digit2}");
