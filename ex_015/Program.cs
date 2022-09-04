5cc72b0cdf01d5552648c14ddbd924078d2c7652
Tatiana Kalinina <tankakalinka@gmail.com>
2022-09-04 22:45:57 +0300

    add hometask 15 DaysOfTheWeek

---
+++ ex_015/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли 
+// этот день выходным.
+
+Console.Write("Input day of the week number from 1 till 7: ");
+int day = int.Parse(Console.ReadLine());
+if (day == 6 || day == 7)
+    Console.Write("YES");
+else
+    Console.Write("NO");
