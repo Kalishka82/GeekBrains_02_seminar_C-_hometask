2d5a0dda9184ca15f57c772381681968dc38871a
Tatiana Kalinina <tankakalinka@gmail.com>
2022-09-06 19:59:04 +0300

    modified according to teacher's comment

---
+++ ex_13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Input any integer number: ");
+int number = int.Parse(Console.ReadLine());
+if (number > 100)
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    int remainder = number % 10;
+    Console.WriteLine($"{remainder}");
+}
+else 
+    {
+        Console.WriteLine("There is no third digit");
+    }
